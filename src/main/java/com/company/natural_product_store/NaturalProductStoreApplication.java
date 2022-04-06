17500c862eff79b3d70416b4786061c324ec52c3
Dinu Alin <dinui.alin@gmail.com>
2022-04-06 21:39:57 +0300

    Initial commit

---
+++ src/main/java/com/company/natural_product_store/NaturalProductStoreApplication.java
@@ -0,0 +1,13 @@
+package com.company.natural_product_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalProductStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaturalProductStoreApplication.class, args);
+    }
+
+}
